3372aedf6aec5e210997f6e143141a15aee9535d
erickconcepcion <erickconcepcion@live.com>
2020-02-12 15:58:11 -0400

    Created header and footer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-
+import {AppRoutingModule} from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   imports:      [ 
     BrowserModule,
+     AppRoutingModule,
      FormsModule,
-     ReactiveFormsModule
+     ReactiveFormsModule,
      ],
-  declarations: [ AppComponent, HelloComponent ],
+  declarations: [ AppComponent, HeaderComponent, FooterComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
